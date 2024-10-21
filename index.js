19acbd5a55048d8acbd8f5dcf29f5b1e3d39c277
Pravin Avhad <122007931+ThalaPravin@users.noreply.github.com>
2024-10-21 13:50:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.use(WithdrawalRequest);
 app.use("../uploads", express.static("uploads"));
 store.sync();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server up and running on port ${PORT}...`);
 });
